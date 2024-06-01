ad1d8ed5f96c55a5eb164cfc7159d70e75d6f0df
AndriyV2504 <154068131+AndriyV2504@users.noreply.github.com>
2024-06-01 20:05:08 +0300

    add-task-12-axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,13 +4,13 @@ const API_KEY = '43967395-ba8c6739f73e8322e41736fdd';
 const BASE_URL = 'https://pixabay.com/api/';
 
 export async function fetchImages(query, page = 1, perPage = 15) {
-    const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
+  const url = `${BASE_URL}?key=${API_KEY}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`;
 
-    try {
-        const response = await axios.get(url);
-        return response.data;
-    } catch (error) {
-        console.error(`Fetch error:`, error);
-        throw error;
-    }
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error("Fetch error:", error);
+    trow error;
+  }
 }
